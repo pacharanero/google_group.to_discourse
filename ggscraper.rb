f179c741244c7be9e9c52cb2adb469b268951485
pacharanero <marcusbaw@gmail.com>
2015-01-19 16:16:08 +0000

    now in OO flavour!

--- ggscraper.rb
+++ ggscraper.rb
@@ -1,21 +1,30 @@
 require 'selenium-webdriver'
 
-#setup
-user = 'pacharanero'
-pass = 'cxn^ka7$OI3w65df3jmTWnaU$N1a3V'
-ccio_url = 'https://accounts.google.com/ServiceLogin?continue=https%3A%2F%2Fgroups.google.com%2Fd%2Fforum%2Fccio&hl=en-GB&service=groups2&passive=true'
+class Ggscraper
 
-#instantiate and navigate
-driver = Selenium::WebDriver.for :firefox
-driver.navigate.to ccio_url
+	def initialize (username, password, url)
+		@username = username
+		@password = password
+		@ccio_url = url
+		@driver = Selenium::WebDriver.for :firefox
+		@driver.navigate.to @ccio_url
+		return @driver
+	end
 
-#login page
-username = driver.find_element(:id, 'Email')
-password = driver.find_element(:id, 'Passwd')
-sign = driver.find_element(:id, 'signIn')
+	def login
+		#find the right elements
+		username_field = @driver.find_element(:id, 'Email')
+		password_field = @driver.find_element(:id, 'Passwd')
+		signin_button = @driver.find_element(:id, 'signIn')
 
-#fill in credentials
-username.send_keys(user)
-password.send_keys(pass)
-sign.click
+		#fill in credentials
+		username_field.send_keys(@username)
+		password_field.send_keys(@password)
+		signin_button.click
+	end
+
+end
+
+
+
 
