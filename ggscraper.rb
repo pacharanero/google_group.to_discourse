320704bf2e925a7412ec75388683ec16e1d60cf4
pacharanero <marcusbaw@gmail.com>
2015-01-28 20:48:33 +0000

    bulk_delete method added

--- ggscraper.rb
+++ ggscraper.rb
@@ -218,6 +218,13 @@ class Ggscraper
     post_parameters
   end
 
+  def bulk_delete(user=ENV['DISCOURSE_API_USER'])
+    connect_discourse if @discourse_client.nil?
+    topics_for_deletion = @discourse_client.topics_by(user) #gets the first 30 topics - this is Discourse's API default
+    topics_for_deletion.each { |id| @discourse_client.delete_topic(id["id"]) }
+    puts "#{topics_for_deletion.count} posts deleted"
+  end  
+
   def close_browsers
     @driver.close
     @raw_driver.close
