a17cab2a08bf3e4d7381eb40eef7eef607201c43
pacharanero <marcusbaw@gmail.com>
2015-02-11 21:21:11 +0000

    tidy up

--- discourse.bulk_delete_topics_by_user.rb
+++
@@ -1,8 +0,0 @@
-#helps with bulk deletion of Discourse topics - useful when you've been experimenting with a google group scrape
-
-def bulk_delete(user=ENV['DISCOURSE_API_USER'])
-  connect_discourse if @discourse_client.nil?
-  topics_for_deletion = @discourse_client.topics_by(user) #gets the first 30 topics - this is Discourse's API default
-  topics_for_deletion.each { |id| @discourse_client.delete_topic(id["id"]) }
-  puts "#{topics_for_deletion.count} posts deleted"
-end  
